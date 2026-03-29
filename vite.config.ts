28aed836e3dcb92443369453296d800d483dc89b
RiazA <riaza@velvetbud.in>
2026-03-29 16:25:16 +0200

    clodflare changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+// import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 9777,
-    allowedHosts: ["in7.velvetbud.in", "xxbrownsnwhites.com", "xwww.brownsnwhites.com"],
+    allowedHosts: ["ai.innovex-om.com", "xxbrownsnwhites.com", "xwww.brownsnwhites.com"],
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+//  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    // REMOVED the mode === "development" line here
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
